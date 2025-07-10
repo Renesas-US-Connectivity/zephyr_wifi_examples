42beb5ec8851f6778754d02296fd7981a9fc6863
Patrick Murphy <patrick.murphy.jy@renesas.com>
2025-07-10 15:56:18 -0400

    udp client sends data, recv not working

--- wifi_sta_udp_client/src/main.c
+++ wifi_sta_udp_client/src/main.c
@@ -65,7 +65,7 @@ int main(void)
     char if_addr_s[NET_IPV4_ADDR_LEN];
     char client_addr_s[INET_ADDRSTRLEN];
 
-	printf("Starting Wi-Fi station UDP server...\n");
+	printf("Starting Wi-Fi station UDP client...\n");
 
 	iface = net_if_get_wifi_sta();
 	if (iface == NULL) {
@@ -115,48 +115,65 @@ int main(void)
 	   DHCP process when WIFI connect function returns. */
 	k_msleep(5000);
 
-	while (1) {
-		sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		if (sfd < 0) {
-			printf("Failed to created socket: %d\n", sfd);
-			return 0;
-		}
-        printf("Socket created: %d\n", sfd);
 
-        server_addr.sin_family = AF_INET;
-    	server_addr.sin_port = htons(SERVER_PORT);
-   	    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR); //htonl(INADDR_ANY);
+    sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sfd < 0) {
+        printf("Failed to created socket: %d\n", sfd);
+        return 0;
+    }
+    printf("Socket created: %d\n", sfd);
 
-  	    /*printf("Binding socket...\n");
-    	if ((bind(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr))) != 0) {
-            printf("Failed to bind socket: %d\n", sfd);
-    		return 0;
-        }*/
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
 
-        while(1) {
+    while(1) {
 
+        bytes_sent = sendto(sfd, 
+                            rx_msg, 
+                            strlen(rx_msg), 
+                            0, 
+                            (struct sockaddr *)&server_addr,
+                            sizeof(server_addr) );
+        if (bytes_sent > 0) {
+            printf("Sent %d bytes: %s\n", bytes_sent, rx_msg);
 
-            /* Echo received data */
-            bytes_sent = sendto(sfd, 
-                                rx_msg, 
-                                strlen(rx_msg), 
-                                0, 
-                                (struct sockaddr *)&server_addr,
-                                sizeof(server_addr) );
-            if (bytes_sent > 0) {
-                printf("Sent %d bytes: %s\n", bytes_sent, rx_msg);
+            /* Listen for resposne */
+            printf("Waitig for resposne...\n");
+            struct sockaddr_in client_addr; 
+            bytes_recvd = recvfrom(sfd, rx_msg, sizeof(rx_msg), 0, &client_addr, sizeof(client_addr));
+            if (bytes_recvd > 0) {
+                /* NULL terminate received data */
+                if (bytes_recvd < RX_MESSAGE_LEN_MAX) {
+                    rx_msg[bytes_recvd] = '\0';
+                }
+                
+                // TODO buf is assuming IPV4, extend for IPV6
+                char buf[INET_ADDRSTRLEN ];
+                net_addr_ntop(client_addr.sin_family, &client_addr.sin_addr, buf, sizeof(buf));
+                // TODO port bytes are swapped. Fix needed in offload driver
+                printf("Received %d bytes from %s:%d : %s\n", bytes_recvd, buf, client_addr.sin_port, rx_msg);
             }
-
+            
             k_msleep(5000);
+        }
+        else {
+            printf("sendto error=%d\n", bytes_sent);
+
+            // dont attempt to read if error
+            break;
         }           
 
-		printf("Closing socket (%d)..\n", sfd);
+        printf("Closing socket (%d)..\n", sfd);
 
-		if (close(sfd) < 0) {
-			printf("Failed to close socket\n");
-			return 0;
-		}
-	}
+        if (close(sfd) < 0) {
+            printf("Failed to close socket\n");
+            return 0;
+        }
+        else {
+            printf("Socket successfully closed\n");
+        }
+    }
 
 	return 0;
 }
