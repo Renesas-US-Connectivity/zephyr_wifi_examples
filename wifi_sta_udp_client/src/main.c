e2ec9715652881083c86a098cfbc3b54b5ac2f5f
Patrick Murphy <patrick.murphy.jy@renesas.com>
2025-07-10 16:58:48 -0400

    udp client send and receive
    Please enter the commit message for your changes. Lines starting

--- wifi_sta_udp_client/src/main.c
+++ wifi_sta_udp_client/src/main.c
@@ -14,11 +14,13 @@
 #define WIFI_SSID				"TP-Link_1218"
 #define WIFI_PSK				"74512829"
 
-/* UDP server configuration */
+/* UDP configuration */
 #define SERVER_ADDR				"192.168.50.55"
 #define SERVER_PORT				53704
+#define CLIENT_PORT				53704
 
 /* Test message configuration */
+#define TX_MESSAGE_LEN_MAX		32
 #define RX_MESSAGE_LEN_MAX		32
 
 static struct net_mgmt_event_callback cb;
@@ -61,6 +63,7 @@ int main(void)
 	struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t client_addr_len;
+    char tx_msg[TX_MESSAGE_LEN_MAX];
 	char rx_msg[RX_MESSAGE_LEN_MAX] = "UDP SERVER LIVES!";
     char if_addr_s[NET_IPV4_ADDR_LEN];
     char client_addr_s[INET_ADDRSTRLEN];
@@ -115,54 +118,64 @@ int main(void)
 	   DHCP process when WIFI connect function returns. */
 	k_msleep(5000);
 
-
-    sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sfd < 0) {
-        printf("Failed to created socket: %d\n", sfd);
-        return 0;
-    }
-    printf("Socket created: %d\n", sfd);
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-
-    while(1) {
-
-        bytes_sent = sendto(sfd, 
-                            rx_msg, 
-                            strlen(rx_msg), 
-                            0, 
-                            (struct sockaddr *)&server_addr,
-                            sizeof(server_addr) );
-        if (bytes_sent > 0) {
-            printf("Sent %d bytes: %s\n", bytes_sent, rx_msg);
-
-            /* Listen for resposne */
-            printf("Waitig for resposne...\n");
-            struct sockaddr_in client_addr; 
-            bytes_recvd = recvfrom(sfd, rx_msg, sizeof(rx_msg), 0, &client_addr, sizeof(client_addr));
-            if (bytes_recvd > 0) {
-                /* NULL terminate received data */
-                if (bytes_recvd < RX_MESSAGE_LEN_MAX) {
-                    rx_msg[bytes_recvd] = '\0';
-                }
-                
-                // TODO buf is assuming IPV4, extend for IPV6
-                char buf[INET_ADDRSTRLEN ];
-                net_addr_ntop(client_addr.sin_family, &client_addr.sin_addr, buf, sizeof(buf));
-                // TODO port bytes are swapped. Fix needed in offload driver
-                printf("Received %d bytes from %s:%d : %s\n", bytes_recvd, buf, client_addr.sin_port, rx_msg);
-            }
-            
-            k_msleep(5000);
+    while (1) {
+        sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (sfd < 0) {
+            printf("Failed to created socket: %d\n", sfd);
+            return 0;
         }
-        else {
-            printf("sendto error=%d\n", bytes_sent);
+        printf("Socket created: %d\n", sfd);
 
-            // dont attempt to read if error
-            break;
-        }           
+        client_addr.sin_family = AF_INET;
+        client_addr.sin_port = htons(CLIENT_PORT);
+        client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        printf("Binding socket...\n");
+        if ((bind(sfd, (struct sockaddr *)&client_addr, sizeof(client_addr))) != 0) {
+            printf("Failed to bind socket: %d\n", sfd);
+            return 0;
+        }
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(SERVER_PORT);
+        server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+        uint32_t seq_nbr = 0;
+        while(1) {
+            snprintf(tx_msg, TX_MESSAGE_LEN_MAX, "Test message %d..!", seq_nbr++);
+            bytes_sent = sendto(sfd, 
+                                tx_msg, 
+                                strlen(tx_msg), 
+                                0, 
+                                (struct sockaddr *)&server_addr,
+                                sizeof(server_addr) );
+            if (bytes_sent > 0) {
+                printf("Sent %d bytes: %s\n", bytes_sent, rx_msg);
+
+                /* Listen for resposne */
+                printf("Waiting for resposne...\n");
+                struct sockaddr_in client_addr; 
+                bytes_recvd = recvfrom(sfd, rx_msg, sizeof(rx_msg), 0, &server_addr, sizeof(server_addr));
+                if (bytes_recvd > 0) {
+                    /* NULL terminate received data */
+                    if (bytes_recvd < RX_MESSAGE_LEN_MAX) {
+                        rx_msg[bytes_recvd] = '\0';
+                    }
+                    
+                    // TODO buf is assuming IPV4, extend for IPV6
+                    char buf[INET_ADDRSTRLEN ];
+                    net_addr_ntop(client_addr.sin_family, &client_addr.sin_addr, buf, sizeof(buf));
+                    // TODO port bytes are swapped. Fix needed in offload driver
+                    printf("Received %d bytes from %s:%d : %s\n", bytes_recvd, buf, client_addr.sin_port, rx_msg);
+                }
+            }
+            else {
+                printf("sendto error=%d\n", bytes_sent);
+
+                // dont attempt to read if error
+                break;
+            } 
+        }
 
         printf("Closing socket (%d)..\n", sfd);
 
