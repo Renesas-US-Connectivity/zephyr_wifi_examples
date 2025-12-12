07670ea2f8d6cc294265b5125b22d695933cfb87
Visakh Venugopal <visakh.venugopal.ym@bp.renesas.com>
2025-12-12 19:47:24 +0530

    Tested HTTP/unsecure client
    Signed-off-by: Visakh Venugopal <visakh.venugopal.ym@bp.renesas.com>

--- http_client/src/main.c
+++ http_client/src/main.c
@@ -1,400 +1,241 @@
 /*
- * Copyright (c) 2019 Intel Corporation
+ * Copyright (c) 2023 Lucas Dietrich <ld.adecy@gmail.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(net_http_client_sample, LOG_LEVEL_DBG);
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
+#include <zephyr/net/dns_resolve.h>
 #include <zephyr/net/tls_credentials.h>
-#include <zephyr/net/http/client.h>
+#include <zephyr/data/json.h>
+#include <zephyr/random/random.h>
+#include <zephyr/logging/log.h>
+//#include "net_sample_common.h"
 
-#include "ca_certificate.h"
 
-#define HTTP_PORT 8000
-#define HTTPS_PORT 4443
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/net/net_if.h>
 
-#if defined(CONFIG_NET_CONFIG_PEER_IPV6_ADDR)
-#define SERVER_ADDR6  CONFIG_NET_CONFIG_PEER_IPV6_ADDR
-#else
-#define SERVER_ADDR6 ""
-#endif
+#include <zephyr/logging/log.h>
 
-#if defined(CONFIG_NET_CONFIG_PEER_IPV4_ADDR)
-#define SERVER_ADDR4  CONFIG_NET_CONFIG_PEER_IPV4_ADDR
-#else
-#define SERVER_ADDR4 ""
-#endif
 
-#define MAX_RECV_BUF_LEN 512
+LOG_MODULE_REGISTER(http_client, LOG_LEVEL_DBG);
 
-static uint8_t recv_buf_ipv4[MAX_RECV_BUF_LEN];
-static uint8_t recv_buf_ipv6[MAX_RECV_BUF_LEN];
+#define SNTP_SERVER "0.pool.ntp.org"
 
-static int setup_socket(sa_family_t family, const char *server, int port,
-			int *sock, struct sockaddr *addr, socklen_t addr_len)
+#define AWS_BROKER_PORT CONFIG_AWS_MQTT_PORT
+
+#define MQTT_BUFFER_SIZE 256u
+#define APP_BUFFER_SIZE	 4096u
+
+#define MAX_RETRIES	    10u
+#define BACKOFF_EXP_BASE_MS 1000u
+#define BACKOFF_EXP_MAX_MS  60000u
+#define BACKOFF_CONST_MS    5000u
+
+#define TLS_TAG_DEVICE_CERTIFICATE 1
+#define TLS_TAG_DEVICE_PRIVATE_KEY 1
+#define TLS_TAG_AWS_CA_CERTIFICATE 2
+
+static const sec_tag_t sec_tls_tags[] = {
+	TLS_TAG_DEVICE_CERTIFICATE,
+	TLS_TAG_AWS_CA_CERTIFICATE,
+};
+
+/* Wi-Fi network configuration */
+/* Wi-Fi network configuration */
+//#define WIFI_SSID				"TP-Link_1218"
+//#define WIFI_PSK				"74512829"
+#define WIFI_SSID				"ITP-FF"
+#define WIFI_PSK				"WiFiNetge@r@1"
+/* TCP server configuration */
+#define SERVER_IP					"192.168.31.224"
+#define SERVER_PORT					10001
+
+/* Test message configuration */
+#define TX_MESSAGE_LEN_MAX			32
+#define RX_MESSAGE_LEN_MAX			32
+
+/* Wi-Fi connection events */
+#define WIFI_EVENT_CONNECT_SUCCESS	BIT(0)
+#define WIFI_EVENT_CONNECT_FAILED	BIT(1)
+#define WIFI_EVENT_ALL				(WIFI_EVENT_CONNECT_SUCCESS | \
+									 WIFI_EVENT_CONNECT_FAILED)
+#define NET_EVENT_ALL				(NET_EVENT_IPV4_ADDR_ADD | \
+									 NET_EVENT_IPV4_DHCP_BOUND)
+
+static void print_wifi_status(struct wifi_iface_status *status);
+
+static struct net_mgmt_event_callback cb;
+static struct net_mgmt_event_callback cb1;
+
+K_EVENT_DEFINE(connect_event);
+K_EVENT_DEFINE(net_event);
+
+static void wifi_event_handler(struct net_mgmt_event_callback *cb,
+				   uint64_t mgmt_event, struct net_if *iface)
 {
-	const char *family_str = family == AF_INET ? "IPv4" : "IPv6";
-	int ret = 0;
+	const struct wifi_status *status = (const struct wifi_status *)cb->info;
 
-	memset(addr, 0, addr_len);
-
-	if (family == AF_INET) {
-		net_sin(addr)->sin_family = AF_INET;
-		net_sin(addr)->sin_port = htons(port);
-		inet_pton(family, server, &net_sin(addr)->sin_addr);
-	} else {
-		net_sin6(addr)->sin6_family = AF_INET6;
-		net_sin6(addr)->sin6_port = htons(port);
-		inet_pton(family, server, &net_sin6(addr)->sin6_addr);
-	}
-
-	if (IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
-		sec_tag_t sec_tag_list[] = {
-			CA_CERTIFICATE_TAG,
-		};
-
-		*sock = socket(family, SOCK_STREAM, IPPROTO_TLS_1_2);
-		if (*sock >= 0) {
-			ret = setsockopt(*sock, SOL_TLS, TLS_SEC_TAG_LIST,
-					 sec_tag_list, sizeof(sec_tag_list));
-			if (ret < 0) {
-				LOG_ERR("Failed to set %s secure option (%d)",
-					family_str, -errno);
-				ret = -errno;
-			}
-
-			ret = setsockopt(*sock, SOL_TLS, TLS_HOSTNAME,
-					 TLS_PEER_HOSTNAME,
-					 sizeof(TLS_PEER_HOSTNAME));
-			if (ret < 0) {
-				LOG_ERR("Failed to set %s TLS_HOSTNAME "
-					"option (%d)", family_str, -errno);
-				ret = -errno;
-			}
+	LOG_INF("Wi-Fi event - layer: %llx code: %llx cmd: %llx status: %d",
+		NET_MGMT_GET_LAYER(mgmt_event), NET_MGMT_GET_LAYER_CODE(mgmt_event),
+		NET_MGMT_GET_COMMAND(mgmt_event), status->status);
+	
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		if (status->status == 0) {
+			LOG_INF("Connected to AP!");
+			k_event_set(&connect_event, WIFI_EVENT_CONNECT_SUCCESS);
+		} else {
+			LOG_INF("Failed to connect to AP!");
+			k_event_set(&connect_event, WIFI_EVENT_CONNECT_FAILED);
 		}
-	} else {
-		*sock = socket(family, SOCK_STREAM, IPPROTO_TCP);
+		break;
+	default:
+		break;
 	}
-
-	if (*sock < 0) {
-		LOG_ERR("Failed to create %s HTTP socket (%d)", family_str,
-			-errno);
-	}
-
-	return ret;
 }
 
-static int payload_cb(int sock, struct http_request *req, void *user_data)
+static void net_event_handler(struct net_mgmt_event_callback *cb,
+				   uint64_t mgmt_event, struct net_if *iface)
 {
-	const char *content[] = {
-		"foobar",
-		"chunked",
-		"last"
-	};
-	char tmp[64];
-	int i, pos = 0;
+	const struct wifi_status *status = (const struct wifi_status *)cb->info;
 
-	for (i = 0; i < ARRAY_SIZE(content); i++) {
-		pos += snprintk(tmp + pos, sizeof(tmp) - pos,
-				"%x\r\n%s\r\n",
-				(unsigned int)strlen(content[i]),
-				content[i]);
+	LOG_INF("NET event - layer: %llx code: %llx cmd: %llx status: %d",
+		NET_MGMT_GET_LAYER(mgmt_event), NET_MGMT_GET_LAYER_CODE(mgmt_event),
+		NET_MGMT_GET_COMMAND(mgmt_event), status->status);
+	
+	switch (mgmt_event) {
+	case NET_EVENT_IPV4_ADDR_ADD:
+		k_event_set(&net_event, NET_EVENT_IPV4_ADDR_ADD);
+		LOG_INF("IPv4 address added");
+		break;
+	case NET_EVENT_IPV4_DHCP_BOUND:
+		k_event_set(&net_event, NET_EVENT_IPV4_DHCP_BOUND);
+		LOG_INF("DHCP bound - we have an IP address!");
+		break;
+	default:
+		break;
 	}
-
-	pos += snprintk(tmp + pos, sizeof(tmp) - pos, "0\r\n\r\n");
-
-	(void)send(sock, tmp, pos, 0);
-
-	return pos;
 }
 
-static int response_cb(struct http_response *rsp,
-		       enum http_final_call final_data,
-		       void *user_data)
+static void dhcp_event_handler(struct net_mgmt_event_callback *cb,
+                              uint32_t mgmt_event, struct net_if *iface)
 {
-	if (final_data == HTTP_DATA_MORE) {
-		LOG_INF("Partial data received (%zd bytes)", rsp->data_len);
-	} else if (final_data == HTTP_DATA_FINAL) {
-		LOG_INF("All the data received (%zd bytes)", rsp->data_len);
-	}
+    if (mgmt_event == NET_EVENT_IPV4_DHCP_BOUND) {
+        LOG_INF("DHCP bound - we have an IP address!");
+        
+        // Get the assigned IP
+        struct in_addr *addr = net_if_ipv4_get_global_addr(iface, NET_ADDR_PREFERRED);
+        if (addr) {
+            char ip_str[NET_IPV4_ADDR_LEN];
+            net_addr_ntop(AF_INET, addr, ip_str, sizeof(ip_str));
+            LOG_INF("IP Address: %s", ip_str);
+        }
 
-	LOG_INF("Response to %s", (const char *)user_data);
-	LOG_INF("Response status %s", rsp->http_status);
-
-	return 0;
+    }
 }
 
-static int connect_socket(sa_family_t family, const char *server, int port,
-			  int *sock, struct sockaddr *addr, socklen_t addr_len)
-{
-	int ret;
-
-	ret = setup_socket(family, server, port, sock, addr, addr_len);
-	if (ret < 0 || *sock < 0) {
-		return -1;
-	}
-
-	ret = connect(*sock, addr, addr_len);
-	if (ret < 0) {
-		LOG_ERR("Cannot connect to %s remote (%d)",
-			family == AF_INET ? "IPv4" : "IPv6",
-			-errno);
-		close(*sock);
-		*sock = -1;
-		ret = -errno;
-	}
-
-	return ret;
-}
-
-static int run_queries(void)
-{
-	struct sockaddr_in6 addr6;
-	struct sockaddr_in addr4;
-	int sock4 = -1, sock6 = -1;
-	int32_t timeout = 3 * MSEC_PER_SEC;
-	int ret = 0;
-	int port = HTTP_PORT;
-	struct http_request req;
-
-	if (IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
-		ret = tls_credential_add(CA_CERTIFICATE_TAG,
-					 TLS_CREDENTIAL_CA_CERTIFICATE,
-					 ca_certificate,
-					 sizeof(ca_certificate));
-		if (ret < 0) {
-			LOG_ERR("Failed to register public certificate: %d",
-				ret);
-			return ret;
-		}
-
-		port = HTTPS_PORT;
-	}
-
-	if (IS_ENABLED(CONFIG_NET_IPV4)) {
-		(void)connect_socket(AF_INET, SERVER_ADDR4, port,
-				     &sock4, (struct sockaddr *)&addr4,
-				     sizeof(addr4));
-		if (sock4 < 0) {
-			LOG_ERR("Cannot create HTTP IPv4 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_GET;
-		req.url = "/";
-		req.host = SERVER_ADDR4;
-		req.protocol = "HTTP/1.1";
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv4;
-		req.recv_buf_len = sizeof(recv_buf_ipv4);
-
-		ret = http_client_req(sock4, &req, timeout, "IPv4 GET");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock4);
-	}
-
-	if (IS_ENABLED(CONFIG_NET_IPV6)) {
-		(void)connect_socket(AF_INET6, SERVER_ADDR6, port,
-				     &sock6, (struct sockaddr *)&addr6,
-				     sizeof(addr6));
-		if (sock6 < 0) {
-			LOG_ERR("Cannot create HTTP IPv6 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_GET;
-		req.url = "/";
-		req.host = SERVER_ADDR6;
-		req.protocol = "HTTP/1.1";
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv6;
-		req.recv_buf_len = sizeof(recv_buf_ipv6);
-
-		ret = http_client_req(sock6, &req, timeout, "IPv6 GET");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock6);
-	}
-
-	sock4 = -1;
-	sock6 = -1;
-
-	if (IS_ENABLED(CONFIG_NET_IPV4)) {
-		(void)connect_socket(AF_INET, SERVER_ADDR4, port,
-				     &sock4, (struct sockaddr *)&addr4,
-				     sizeof(addr4));
-		if (sock4 < 0) {
-			LOG_ERR("Cannot create HTTP IPv4 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_POST;
-		req.url = "/foobar";
-		req.host = SERVER_ADDR4;
-		req.protocol = "HTTP/1.1";
-		req.payload = "foobar";
-		req.payload_len = strlen(req.payload);
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv4;
-		req.recv_buf_len = sizeof(recv_buf_ipv4);
-
-		ret = http_client_req(sock4, &req, timeout, "IPv4 POST");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock4);
-	}
-
-	if (IS_ENABLED(CONFIG_NET_IPV6)) {
-		(void)connect_socket(AF_INET6, SERVER_ADDR6, port,
-				     &sock6, (struct sockaddr *)&addr6,
-				     sizeof(addr6));
-		if (sock6 < 0) {
-			LOG_ERR("Cannot create HTTP IPv6 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_POST;
-		req.url = "/";
-		req.host = SERVER_ADDR6;
-		req.protocol = "HTTP/1.1";
-		req.payload = "foobar";
-		req.payload_len = strlen(req.payload);
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv6;
-		req.recv_buf_len = sizeof(recv_buf_ipv6);
-
-		ret = http_client_req(sock6, &req, timeout, "IPv6 POST");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock6);
-	}
-
-	/* Do a chunked POST request */
-
-	sock4 = -1;
-	sock6 = -1;
-
-	if (IS_ENABLED(CONFIG_NET_IPV4)) {
-		const char *headers[] = {
-			"Transfer-Encoding: chunked\r\n",
-			NULL
-		};
-
-		(void)connect_socket(AF_INET, SERVER_ADDR4, port,
-				     &sock4, (struct sockaddr *)&addr4,
-				     sizeof(addr4));
-		if (sock4 < 0) {
-			LOG_ERR("Cannot create HTTP IPv4 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_POST;
-		req.url = "/chunked-test";
-		req.host = SERVER_ADDR4;
-		req.protocol = "HTTP/1.1";
-		req.payload_cb = payload_cb;
-		req.header_fields = headers;
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv4;
-		req.recv_buf_len = sizeof(recv_buf_ipv4);
-
-		ret = http_client_req(sock4, &req, timeout, "IPv4 POST");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock4);
-	}
-
-	if (IS_ENABLED(CONFIG_NET_IPV6)) {
-		const char *headers[] = {
-			"Transfer-Encoding: chunked\r\n",
-			NULL
-		};
-
-		(void)connect_socket(AF_INET6, SERVER_ADDR6, port,
-				     &sock6, (struct sockaddr *)&addr6,
-				     sizeof(addr6));
-		if (sock6 < 0) {
-			LOG_ERR("Cannot create HTTP IPv6 connection.");
-			return -ECONNABORTED;
-		}
-
-		memset(&req, 0, sizeof(req));
-
-		req.method = HTTP_POST;
-		req.url = "/chunked-test";
-		req.host = SERVER_ADDR6;
-		req.protocol = "HTTP/1.1";
-		req.payload_cb = payload_cb;
-		req.header_fields = headers;
-		req.response = response_cb;
-		req.recv_buf = recv_buf_ipv6;
-		req.recv_buf_len = sizeof(recv_buf_ipv6);
-
-		ret = http_client_req(sock6, &req, timeout, "IPv6 POST");
-		if (ret < 0) {
-			LOG_ERR("Client error %d", ret);
-		}
-
-		close(sock6);
-	}
-
-	return ret;
-}
+int http_client(void);
 
 int main(void)
 {
-	int iterations = CONFIG_NET_SAMPLE_SEND_ITERATIONS;
-	int i = 0;
-	int ret = 0;
+	struct net_if *iface;
+    struct in_addr *if_addr;
+	struct wifi_connect_req_params config = {0};
+	struct wifi_iface_status status = {0};
+	struct wifi_version version = {0};
 
-	while (iterations == 0 || i < iterations) {
-		ret = run_queries();
-		if (ret < 0) {
-			ret = 1;
-			break;
+	uint32_t events;
+
+	LOG_INF("Starting HTTP client...");
+	k_msleep(3000);
+	iface = net_if_get_wifi_sta();
+	if (iface == NULL) {
+		LOG_INF("Cannot find the Wi-Fi interface");
+		return 0;
+	}
+
+	LOG_INF("iface found\n");
+	net_mgmt_init_event_callback(&cb, wifi_event_handler, 
+			NET_EVENT_WIFI_CONNECT_RESULT);
+	net_mgmt_add_event_callback(&cb);
+
+	net_mgmt_init_event_callback(&cb1, net_event_handler, 
+			 NET_EVENT_IPV4_ADDR_ADD | NET_EVENT_IPV4_DHCP_BOUND);
+	net_mgmt_add_event_callback(&cb1);
+
+	LOG_INF("callback registered\n");
+	if (net_mgmt(NET_REQUEST_WIFI_VERSION, iface, &version,
+			sizeof(version)) == 0) {
+		LOG_INF("Wi-Fi Driver Version: %s", version.drv_version);
+		LOG_INF("Wi-Fi Firmware Version: %s", version.fw_version);
+	}
+
+	LOG_INF("version get success\n");
+	config.ssid = (const uint8_t *)WIFI_SSID;
+	config.ssid_length = strlen(WIFI_SSID);
+	config.psk = (const uint8_t *)WIFI_PSK;
+	config.psk_length = strlen(WIFI_PSK);
+	config.security = WIFI_SECURITY_TYPE_PSK;
+	config.channel = WIFI_CHANNEL_ANY;
+	config.band = WIFI_FREQ_BAND_2_4_GHZ;
+
+	do {
+		LOG_INF("Connecting to network (SSID: %s)", config.ssid);
+
+		if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &config,
+					sizeof(struct wifi_connect_req_params))) {
+			LOG_INF("Wi-Fi connect request failed");
+			return 0;
 		}
 
-		if (iterations > 0) {
-			i++;
-			if (i >= iterations) {
-				ret = 0;
-				break;
-			}
+		events = k_event_wait(&connect_event, WIFI_EVENT_ALL, true, K_FOREVER);	
+		if (events == WIFI_EVENT_CONNECT_SUCCESS) {
+			LOG_INF("Joined network!");
+			break;
+		}
+	} while (1);
+
+	do {
+		events = k_event_wait(&net_event, NET_EVENT_ALL, true, K_FOREVER);	
+		if (events & NET_EVENT_IPV4_DHCP_BOUND) {
+			LOG_INF("DHCP lease received!\n");
+			break;
+		}
+	} while (1);
+
+#if defined (CONFIG_SHIELD_RENESAS_QCIOT_RRQ61051EVZ_PMOD)
+	do {
+		LOG_INF("Waiting for IP address to be assigned...\n");
+
+		if_addr = net_if_ipv4_get_global_addr(iface, NET_ADDR_PREFERRED);
+
+		if (if_addr) {
+			net_addr_ntop(AF_INET, if_addr->s4_addr, if_addr_s, sizeof(if_addr_s));
+			LOG_INF("Address: %s", if_addr_s);
 		} else {
-			ret = 0;
-			break;
-		}
+			k_msleep(1000);
+		}		
+	} while (if_addr == NULL);
+#else
+	k_msleep(3000);
+#endif
+
+	LOG_INF("waiting to wifi success\n");
+	//wait_for_network();
+
+	for (;;) {
+		http_client();
+		k_sleep(K_SECONDS(10));
 	}
 
-	if (iterations == 0) {
-		k_sleep(K_FOREVER);
-	}
-
-	exit(ret);
-	return ret;
+	return 0;
 }
